7fdad3d3efc7904e55f534673926f7b94fdf0f30
SeHyun <rla6170@naver.com>
2024-07-21 02:00:21 +0900

    feat-init : homepage 이름 변경

---
+++ src/pages/company/CompanyHomePage.jsx
@@ -0,0 +1,5 @@
+const CompanyHomePage = () => {
+  return <div></div>;
+};
+
+export default CompanyHomePage;
---
+++ src/pages/user/UserHomePage.jsx
@@ -0,0 +1,9 @@
+const UserHomePage = () => {
+  return (
+    <div>
+      <h1>url 직접 조정하면서 작업해주세요~</h1>
+    </div>
+  );
+};
+
+export default UserHomePage;
