f451bede53947a5031bd0ec4111709d5414ccb04
SeHyun <rla6170@naver.com>
2024-07-21 22:59:23 +0900

    feat-select : selectAction 컴포넌트 추가(navigate state 변경 예정

---
+++ src/styles/components/selectAction.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 410px;
+    height: 500px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    gap: 48px;
+
+    & > span {
+        font-size: 24px;
+        font-weight: 700;
+    }
+`;
+
+const RadiusBigBtn = styled.div`
+  width: 410px;
+  height: 410px;
+
+  border: 4px solid ${(props) => props.theme.color.black};
+  border-radius: 50%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+
+  & > span {
+    font-size: 32px;
+    font-weight: 500;
+  }
+`;
+
+export { Container, RadiusBigBtn };
