b71fdddcae0aca1030e0fb7b8d7be49671d29559
SeHyun <rla6170@naver.com>
2024-07-23 16:45:35 +0900

    feat-userhome : menu 정보 객체 분리

---
+++ src/constans/MenuData.js
@@ -0,0 +1,50 @@
+export const userMenu = {
+  key: "user",
+  data: [
+    {
+      name: "뮤지컬",
+      locate: "",
+    },
+    {
+      name: "연극",
+      locate: "",
+    },
+    {
+      name: "콘서트",
+      locate: "",
+    },
+    {
+      name: "무용/클래식",
+      locate: "",
+    },
+    {
+      name: "전시/행사",
+      locate: "",
+    },
+  ],
+};
+export const companyMenu = {
+  key: "company",
+  data: [
+    {
+      name: "의상제작",
+      locate: "",
+    },
+    {
+      name: "중고 판매",
+      locate: "",
+    },
+    {
+      name: "중고 대여",
+      locate: "",
+    },
+    {
+      name: "재활용 판매",
+      locate: "",
+    },
+    {
+      name: "공연 제작자 등록",
+      locate: "",
+    },
+  ],
+};
