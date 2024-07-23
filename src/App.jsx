9d0de77c10f580c185017b8e461b63a93507700f
Yean <kjan0929@naver.com>
2024-07-23 23:35:59 +0900

    chore-route: 라우팅 수정 및 검색 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CompanyHomePage from "./pages/company/CompanyHomePage";
 import UserHomePage from "./pages/user/UserHomePage";
 import RequestItemPage from "./pages/company/RequestItemPage";
 import SelectActionPage from "./pages/company/SelectActionPage";
+import SearchItemPage from "./pages/company/SearchItemPage";
 import Layout from "./components/layout/Layout";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/globalStyle";
@@ -13,18 +14,22 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-      <GlobalStyle/>     
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<UserHomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/company" element={<CompanyHomePage />}/>
-            <Route path="/company/select" element={<SelectActionPage />} />
-            <Route path="/company/request" element={<RequestItemPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<UserHomePage />} />
+              <Route path="login" element={<LoginPage />} />
+
+              <Route path="/company">
+                <Route index element={<CompanyHomePage />} />
+                <Route path="/company/select" element={<SelectActionPage />} />
+                <Route path="/company/request" element={<RequestItemPage />} />
+                <Route path="/company/search" element={<SearchItemPage />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
   );
---
+++ src/pages/company/SearchItemPage.jsx
@@ -0,0 +1,5 @@
+const SearchItemPage = () => {
+  return <h1>검색 페이지입니다</h1>;
+};
+
+export default SearchItemPage;
