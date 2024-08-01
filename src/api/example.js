b686418e172dc4c4b50c39ea5c4da81fa46cd6c8
SeHyun <rla6170@naver.com>
2024-08-01 10:57:08 +0900

    chore-init : axios example jsx 사용 추가

--- src/api/example.js
+++ src/api/example.js
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import instance from "./instance";
 
 export const getUsername = async() => {
@@ -5,10 +6,24 @@ export const getUsername = async() => {
     return response.data;
 }
 
+// jsx에서 사용 
+const handleGetname = async () => {
+    try {
+      const res = await getUsername();
+      if (res.data.success) {
+        // success logic
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        // error logic
+      }
+    }
+  };
+
 /* export const [methodType + apiName] = async ({ requestData : optional }) => {
     const response = await instance.get[put,post,delete...]('url', requestData);
-    return response.data; <- status 등이 필요하면 response 만 받아서 사용해도 무관
+    return response; <- data만 필요하면 response.data를 return으로 사용해도 무관
 }
 */
 
-// fileName은 비슷한 api끼리 묶어서
+// fileName은 비슷한 api끼리 묶어서 적합한 단어로 저장해주세요
