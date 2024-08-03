8db1e35585fddc077be71c9bdabdadd446a1d8f1
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2024-08-03 22:33:04 +0900

    회원가입 api연동

--- src/components/form/Label.jsx
+++ src/components/form/Label.jsx
@@ -1,11 +1,12 @@
-const Label = ({ children, label }) => {
-    return (
-      <div style={{ display: "block", position:"relative", flexDirection: "column", lineheight: "140%" }}>
-        <span style={{ fontSize: "16px", fontWeight: 500 }}>{label}</span>
-        {children}
-      </div>
-    );
-  };
-  
-  export default Label;
-  
+const Label = ({ children, label, valid }) => {
+  return (
+    <div style={{ display: "block", position:"relative", flexDirection: "column", lineheight: "140%", marginBottom:"24px" }}>
+      <span style={ valid ? { fontSize: "16px", fontWeight: 500, color: "red"} : { fontSize: "16px", fontWeight: 500} }>
+        {valid ? label + "*" : label}</span>
+      {children}
+      {valid && <div style={{ color: "red", fontSize: "14px", margin:"8px 4px"}}>중복된 닉네임입니다.</div>}
+    </div>
+  );
+};
+
+export default Label;
