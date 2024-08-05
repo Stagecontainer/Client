569d128848e0135b274ec8849007c1b9ec7c4cc4
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2024-08-05 22:42:05 +0900

    닉네임 제거

--- src/components/form/Label.jsx
+++ src/components/form/Label.jsx
@@ -4,7 +4,7 @@ const Label = ({ children, label, valid }) => {
       <span style={ valid ? { fontSize: "16px", fontWeight: 500, color: "red"} : { fontSize: "16px", fontWeight: 500} }>
         {valid ? label + "*" : label}</span>
       {children}
-      {valid && <div style={{ color: "red", fontSize: "14px", margin:"8px 4px"}}>중복된 닉네임입니다.</div>}
+      {valid && <div style={{ color: "red", fontSize: "14px", margin:"8px 4px"}}>중복된 아이디입니다.</div>}
     </div>
   );
 };
